8e959597ba2f02eca5352e8dfde5073ea7ed0c03
pawnwlw <1239048739@qq.com>
2019-09-09 20:09:21 +0800

    v1 框架

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let input = "";//输入
+let res = "";//输出
+let num = 0;
+let variable = new boolean[26];
+let resultOr ;
+let resultAnd;
+let isNot = new boolean[26];
+
+
+let cal =  new function(){//监听确定按钮
+    //清零操作
+    if(eval(input));// resultOr.add();
+    else ;//resultAnd.add();
+    //……打印
+}
+
+let addX = new function(){
+    num++;
+    //显示……
+}
+
+//let deleteX ……
+
